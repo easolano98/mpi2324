f29bc7f34bb6136ea47f9c7944ccbc5da22f27d6
Erick Solano <solano_erick@hotmail.com>
2023-12-19 13:52:06 +0000

    ejmplo01

---
+++ mpiEj1.cpp
@@ -0,0 +1,14 @@
+
+#include <mpi.h>
+
+int main(int argc, char ** argv){
+    MPI_Init(&argc, &argv);
+    int rank, nprocs;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+    std::printf("rank %d de %d\n", rank, nprocs);
+
+    MPI_Finalize();
+
+}
